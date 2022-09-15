4478d3409ccd93996a63185e5b1f3d7b6654a187
cliff.folsom <cliff.folsom@decodeadvertising.com>
2022-09-15 10:47:09 -0500

    check

--- src/App.js
+++ src/App.js
@@ -1,22 +1,12 @@
-import logo from './logo.svg';
 import './App.css';
+import {BsStopCircle} from 'react-icons/bs'
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+      <h1>Learn this shit!!!</h1>
+      <BsStopCircle />
       </header>
     </div>
   );
